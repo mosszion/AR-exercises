4065e53965b144c3c08dce51a063aba599617997
Mussie <musdzion@gmail.com>
2024-07-30 10:19:39 -0400

    update the stores

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+#Assign the variables 
+@store1 = Store.find_by(id:1)
+@store2 = Store.find_by(name: "Richmond")
+@store3 = Store.find_by(id:3)
+
+#update the variables
+@store1.update(annual_revenue: 500000)
+@store2.update(mens_apparel: true)
+@store3.update(name:"Gastowm")
