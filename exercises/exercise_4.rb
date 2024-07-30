a318cfbc99426c0130a8ef3f85c2d83438b5d4fa
Mussie <musdzion@gmail.com>
2024-07-30 11:19:47 -0400

    add comments

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,11 +11,16 @@ Store.create(name:"Surrey" , annual_revenue: 224000, mens_apparel:false, womens_
 Store.create(name:"Whistler" , annual_revenue: 1900000, mens_apparel:true, womens_apparel:false)
 Store.create(name:"Yaletown" , annual_revenue: 430000, mens_apparel:true, womens_apparel:true)
 
+#sets a variable to keep sotres that carry men's appareal
+#And loop out thier names
 @mens_stores=Store.mens_apparel_stores
 
 @mens_stores.each do |store|
   puts store.name
 end
+
+#define a variable to carry the stores and loop out thier names
+#This code filters stores that carry womesn appreal and thier revenue is less than 1M
 @womens_stores=Store.womens_apparel_stores
 
 @womens_stores.each do |store|
