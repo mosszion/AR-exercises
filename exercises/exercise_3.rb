e1a03a1168f8771694df542b01e00c4aaa6685cf
Mussie <musdzion@gmail.com>
2024-07-30 10:30:12 -0400

    delete third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,10 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+#first assign to a variable gastown
+gastown = Store.find_by(name:"Gastowm")
+#delete the Gastown store
+gastown.destroy
+
+
+
