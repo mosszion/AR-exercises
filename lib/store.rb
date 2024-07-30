0bc1dec90a2978433b42d92327cdb0626610184c
Mussie <musdzion@gmail.com>
2024-07-30 11:20:52 -0400

    define a method which filters stores that carry womens appareal generating less than 1M revenue

--- lib/store.rb
+++ lib/store.rb
@@ -2,5 +2,8 @@ class Store < ActiveRecord::Base
   def self.mens_apparel_stores
     where(mens_apparel: true)
   end
+  def self.womens_apparel_stores
+    where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+  end
 
 end
